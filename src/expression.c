107f8ae68cd7ebc572834910cdf8173c9ba7c88a
Alfredo Monteiro <amonteiro.alfredo@gmail.com>
2025-02-11 10:53:14 -0100

    refactor: parser

--- src/expression.c
+++ src/expression.c
@@ -55,6 +55,77 @@ Expression *expression_allocate_binary(ExpressionKind kind, Expression *left_ope
     return expression;
 }
 
+void expression_print(Expression *expression)
+{
+
+    if (expression->kind == ExpressionKind_Number)
+    {
+        ExpressionNumber number = expression->number;
+        printf("%.1f", number.value);
+    }
+    else if (expression->kind == ExpressionKind_Negation)
+    {
+        ExpressionNegation negation = expression->negation;
+        printf("(");
+        printf("-");
+        expression_print(negation.operand);
+        printf(")");
+    }
+    else if (expression->kind == ExpressionKind_Grouping)
+    {
+        ExpressionGrouping grouping = expression->grouping;
+        expression_print(grouping.expression);
+    }
+    else if (expression->kind == ExpressionKind_Addition)
+    {
+        ExpressionAddition addition = expression->addition;
+        printf("(");
+        expression_print(addition.left_operand);
+        printf(" + ");
+        expression_print(addition.right_operand);
+        printf(")");
+    }
+    else if (expression->kind == ExpressionKind_Subtraction)
+    {
+        ExpressionSubtraction subtraction = expression->subtraction;
+        printf("(");
+        expression_print(subtraction.left_operand);
+        printf(" - ");
+        expression_print(subtraction.right_operand);
+        printf(")");
+    }
+    else if (expression->kind == ExpressionKind_Multiplication)
+    {
+        ExpressionMultiplication multiplication = expression->multiplication;
+        printf("(");
+        expression_print(multiplication.left_operand);
+        printf(" * ");
+        expression_print(multiplication.right_operand);
+        printf(")");
+    }
+    else if (expression->kind == ExpressionKind_Division)
+    {
+        ExpressionDivision division = expression->division;
+        printf("(");
+        expression_print(division.left_operand);
+        printf(" / ");
+        expression_print(division.right_operand);
+        printf(")");
+    }
+    else if (expression->kind == ExpressionKind_Exponentiation)
+    {
+        ExpressionExponentiation exponentiation = expression->exponentiation;
+        printf("(");
+        expression_print(exponentiation.left_operand);
+        printf(" ^ ");
+        expression_print(exponentiation.right_operand);
+        printf(")");
+    }
+    else
+    {
+        printf("Error: node not supported!");
+    }
+}
 void expression_free(Expression *expression)
 {
     if (expression->kind == ExpressionKind_Number)
